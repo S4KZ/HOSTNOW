844c1077bcc2d342ee6f91a2c3ae75c00aba9d7f
Samuel Vinícius <166819602+S4KZ@users.noreply.github.com>
2025-01-05 20:19:06 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página de Exemplo</title>
+    <link rel="stylesheet" href="botao.css">
+</head>
+<body>
+    <div class="containerbtn">
+        <div class="image">
+            <img src="logo2.png" height="300px" width="360px" alt="Imagem">
+        </div>
+
+        <div class="description">
+            <h1>Bem-Vindos ao HostNow!</h1>
+              <p><strong>Hostnow</strong> é um projeto desenvolvido pelos alunos de Desenvolvimento de sistemas com auxilio do professor Jonhson.<br>
+              O projeto visa auxiliar a administração de hospedagem de um forma mais eficaz e intuitiva,<br>
+               também auxiliar o cliente no processo de reservas de uma forma mais fácil e sem complicações</p>
+        </div>
+        <div class="buttons">
+            <a href="./hostnowUser" class="button">Acessar como Usuário</a>
+            <a href="./HostnowFunc/" class="button">Acessar como Funcionário</a>
+        </div>
+    </div>
+</body>
+</html>
